7f717dadba03e48cce94f0c43c1437ef978b6765
mengyer34 <mengyi.yoeng34@gmail.com>
2022-06-27 15:55:45 +0700

    set variable of each class

--- Restaurant/src/OrderManager/order/Order.ts
+++ Restaurant/src/OrderManager/order/Order.ts
@@ -1,1 +1,11 @@
-    
+import { Table } from "../table/Table";
+
+export class Order {
+    protected price: number = 0;
+    protected orderItem: OrderItemp[] = [];
+    constructor(private number: number, private table: Table, private servingWaiter: Waiter) {}
+
+    setPay(price: number) {
+        this.price = price;
+    } 
+}
